a876e373cd4c5dda9294a091fb6790f8be658d8c
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-27 01:16:38 +0200

    Adapted further metainfo changes

--- mopacparser/metainfo/__init__.py
+++ mopacparser/metainfo/__init__.py
@@ -16,4 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import mopacparser.metainfo.mopac
+from nomad.metainfo import Environment
+
+from . import mopac
+
+m_env = Environment()
+m_env.m_add_sub_section(Environment.packages, mopac.m_package)
